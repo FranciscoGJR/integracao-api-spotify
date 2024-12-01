363724c3a8a25abe86d0e75700b57c30a9196250
FranciscoGJR <franciscoogjr@gmail.com>
2024-12-01 16:40:01 -0300

    refactor: constantes

---
+++ api/src/main/java/com/api/Constantes.java
@@ -0,0 +1,13 @@
+package com.api;
+
+public class Constantes {
+	public static String ACCESS_TOKEN = "BQBuFhcrj7yGasweoqAuCTpkEzpN9hIxZgNUWODI_33FVmTqZlu3KHmdVfnheAkxBGvy_e4QjtcgrCDd8J9X1EOfFABDmioIWDJmQqMt6v2bi_Ip0ORsESLREwCdn7w8_UgL8opEk2tc4SCD8wboxA5zLx7ijQzLXVecn9biFYJyWwiJXDmWivufRQKQsbp3r_vaz0coX2rmOgrFEIltcItY-WFF5KPu";
+
+	public static final String URL_API_SPOTIFY = "https://api.spotify.com/v1/artists/";
+
+	public static final String URL_API_SPOTIFY_CHARTS = "https://charts-spotify-com-service.spotify.com/auth/v0/charts/";
+
+	public static final String PATH_CITY = "citytoptrack-belem-weekly/2024-11-28";
+
+	public static final String ID_ARTISTA_EXEMPLO = "0TnOYISbd1XYRBk9myaseg";
+}
