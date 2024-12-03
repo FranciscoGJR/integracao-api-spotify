f22018159598c8b8d2db84b2555a7054b0e56a87
thais-fujissaka <80991615+thais-fujissaka@users.noreply.github.com>
2024-12-03 00:23:47 -0300

    musicas com mais de um genero e alteracao na classificacao de popularidade

--- api/src/main/java/com/api/Constantes.java
+++ api/src/main/java/com/api/Constantes.java
@@ -1,7 +1,7 @@
 package com.api;
 
 public class Constantes {
-	public static String ACCESS_TOKEN = "BQBRmIb1rVi9oUxEJKmRwHa2wImwWGSRsppRooP3elMP6b22OND0yBAIo2tXmS5-YEbU62BFHYnwy33UR1LYfOdqF1VrxvkYaSd1LVTWeFCKgptjJi8J0HTGrCorCRxLIrskMEa4TcWrmf6_vPWKJpChW8KvEg1VNOZ5B6pBiSM7jNqjzhK-3m796Q7P8uztuZ32oqMjyFd7DXg2B-hVkA1ON-Nsh2Lx";
+	public static String ACCESS_TOKEN = "BQDdkTjjXx2ySl8evDRz1WKLNEMg-toxJ1pVrqiLQQPToi2cEpsU0ndqzFhvuKDj3cBjowbV9SmUmDizxfL5TnrYTe0JfvFynpW3gsrhKC5g7hDHuusLEu2H6NKOuJAeImdSFkmel9DhDuXmSrWuF5Fvqsbt9xAhxCIi4hcedGoOJDfiVVIRztNFMnMa6Q9ksetqh8lKwnbG0ggzMaphqh1OCE_C0fT2";
 	
 	public static final String URL_API_SPOTIFY = "https://api.spotify.com/v1/artists/";
 
